0e1ed27d911a33c6372cb9095dfb83896e974dfb
Charlyln <charleshenry.lenue>
2020-07-18 13:52:51 +0200

    add a route in router to new page posts

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import "./index.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Wall from "./components/App/Wall";
 import SignUp from "./components/Signup/SignUp";
+import Users from "./components/App/Users";
+import Posts from "./components/App/Posts";
 
 const App = () => {
   return (
     <BrowserRouter>
-      
       <Switch>
         <Route exact path="/" component={SignUp} />
-        <Route path="/wall" component={Wall} />
-        {/* <Route path="/posts" component={Posts} /> */}
+        <Route path="/wall" component={Posts} />
+        <Route path="/users" component={Users} />
+        <Route path="/messenger" component={Wall} />
         {/* <Route path="/tableScore" component={TableScore} /> */}
       </Switch>
     </BrowserRouter>
