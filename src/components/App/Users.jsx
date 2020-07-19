07c84c9843e4509f651232265b37e0176477cd1c
Charlyln <charleshenry.lenue>
2020-07-19 17:16:36 +0200

    add help alert, adjust grid and  add skeleton

--- src/components/App/Users.jsx
+++ src/components/App/Users.jsx
@@ -6,6 +6,8 @@ import MyAppBar from "./AppBar/MyAppBar";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import HelpIcon from "@material-ui/icons/Help";
+
 import {
   Paper,
   ListItemAvatar,
@@ -13,7 +15,12 @@ import {
   MuiThemeProvider,
   Grid,
   Button,
+  IconButton,
+  Snackbar,
 } from "@material-ui/core";
+import { Slide } from "react-reveal";
+import Skeleton from "@material-ui/lab/Skeleton";
+import Alert from "@material-ui/lab/Alert";
 
 export default function Users() {
   const [isLoading, setIsLoading] = useState(true);
@@ -22,6 +29,9 @@ export default function Users() {
   const [userdata, setuserdata] = useState([]);
   const [follow, setFollow] = useState("");
   const [followId, setFollowId] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   useEffect(() => {
     getUsers();
@@ -32,9 +42,21 @@ export default function Users() {
     try {
       const res = await Axios.get(`${apiUrl}/users`);
       setusersdata(res.data);
+      const timer = setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+      return () => clearTimeout(timer);
     } catch (err) {
       console.log(err);
     }
+  };
+
+  const openInfos = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    setOpen(false);
   };
 
   const getUser = async () => {
@@ -80,69 +102,117 @@ export default function Users() {
   return (
     <>
       <MyAppBar />
-      <MuiThemeProvider>
+
+      <Grid container alignItems="center" style={{ marginTop: "70px" }}>
+        <Grid container>
+          <Grid item xs={12} style={{ textAlign: "end" }}>
+            <IconButton>
+              <HelpIcon onClick={openInfos} />
+            </IconButton>
+          </Grid>
+        </Grid>
+
         <Grid container alignItems="center" style={{ height: "100%" }}>
           <Grid item xs={12}>
             <Grid container alignItems="center" justify="center">
-              <List style={{ width: "500px" }}>
-                {usersdata
-                  .sort(function (a, b) {
-                    return new Date(a.createdAt) - new Date(b.createdAt);
-                  })
-                  .map((user) => (
-                    <Paper elevation={4} style={{ margin: 32, width: "300px" }}>
-                      <ListItem alignItems="flex-start">
-                        <ListItemAvatar>
-                          <Avatar alt="Temy Sharp" src={user.avatar} />
-                        </ListItemAvatar>
-                        <ListItemText
-                          primary={user.pseudo}
-                          secondary={`${user.Followers.length} ${
-                            user.Followers.length < 2 ? "follower" : "followers"
-                          }`}
-                        />
+              {isLoading ? (
+                <>
+                  <List style={{ width: "500px", marginTop:"17px" }}>
+                    <Slide top cascade>
+                      {array.map((el) => (
+                        <ListItem alignItems="flex-start">
+                          <Skeleton
+                            variant="rect"
+                            width={300}
+                            height={72}
+                            className="usersCards"
+                            style={{ margin: "8px 16px" }}
+                          ></Skeleton>
+                        </ListItem>
+                      ))}
+                    </Slide>
+                  </List>
+                </>
+              ) : (
+                <List style={{ width: "500px" }}>
+                  {usersdata
+                    .filter((user) => user.uuid !== UserId)
+                    .sort(function (a, b) {
+                      return new Date(a.createdAt) - new Date(b.createdAt);
+                    })
+                    .map((user) => (
+                      <Paper
+                        elevation={4}
+                        style={{ margin: 32, width: "300px" }}
+                      >
+                        <ListItem alignItems="flex-start">
+                          <ListItemAvatar>
+                            <Avatar alt="Temy Sharp" src={user.avatar} />
+                          </ListItemAvatar>
+                          <ListItemText
+                            primary={user.pseudo}
+                            secondary={`${user.Followers.length} ${
+                              user.Followers.length < 2
+                                ? "follower"
+                                : "followers"
+                            }`}
+                          />
 
-                        {user.Followers.find(
-                          (follower) => follower.followerId === UserId
-                        ) ? (
-                          <form onSubmit={deleteFollow}>
-                            <Button
-                              onClick={() =>
-                                setFollowId(
-                                  user.Followers.find(
-                                    (follow) => follow.followerId === UserId
+                          {user.Followers.find(
+                            (follower) => follower.followerId === UserId
+                          ) ? (
+                            <form onSubmit={deleteFollow}>
+                              <Button
+                                onClick={() =>
+                                  setFollowId(
+                                    user.Followers.find(
+                                      (follow) => follow.followerId === UserId
+                                    )
                                   )
-                                )
-                              }
-                              size="small"
-                              variant="contained"
-                              color="secondary"
-                              type="submit"
-                            >
-                              I Follow
-                            </Button>
-                          </form>
-                        ) : (
-                          <form onSubmit={postFollow}>
-                            <Button
-                              onClick={() => setFollow(user.uuid)}
-                              size="small"
-                              variant="contained"
-                              color="primary"
-                              type="submit"
-                            >
-                              Follow
-                            </Button>
-                          </form>
-                        )}
-                      </ListItem>
-                    </Paper>
-                  ))}
-              </List>
+                                }
+                                size="small"
+                                variant="contained"
+                                color="secondary"
+                                type="submit"
+                              >
+                                I Follow
+                              </Button>
+                            </form>
+                          ) : (
+                            <form onSubmit={postFollow}>
+                              <Button
+                                onClick={() => setFollow(user.uuid)}
+                                size="small"
+                                variant="contained"
+                                color="primary"
+                                type="submit"
+                              >
+                                Follow
+                              </Button>
+                            </form>
+                          )}
+                        </ListItem>
+                      </Paper>
+                    ))}
+                </List>
+              )}
             </Grid>
           </Grid>
         </Grid>
-      </MuiThemeProvider>
+      </Grid>
+      <Snackbar
+        open={open}
+        autoHideDuration={10000}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity="info" style={{ width: "330px" }}>
+         Here you can follow people you want and see their posts on the wall ! I suggest you <strong>Dark Vador</strong> and <strong>Homer Simpson</strong>, funny guys{" "}
+          <span role="img" aria-label="donut">
+            😀
+          </span>
+        </Alert>
+      </Snackbar>
     </>
   );
 }
