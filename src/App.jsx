fe80a9ccb545d9c5915746f48d73e2ff5c76cca8
Charlyln <charleshenry.lenue>
2020-07-19 17:09:20 +0200

    add post route and crash route

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import "./index.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Wall from "./components/App/Wall";
 import SignUp from "./components/Signup/SignUp";
 import Users from "./components/App/Users";
 import Posts from "./components/App/Posts";
+import Messenger from "./components/App/Messenger";
+import Crash from "./Crash";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={SignUp} />
-        <Route path="/wall" component={Posts} />
+        {/* <Route exact path="/" component={Crash} /> */}
+        <Route path="/posts" component={Posts} />
         <Route path="/users" component={Users} />
-        <Route path="/messenger" component={Wall} />
-        {/* <Route path="/tableScore" component={TableScore} /> */}
+        <Route path="/messenger" component={Messenger} />
       </Switch>
     </BrowserRouter>
   );
